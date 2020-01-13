1f93ffbb9e9c7b84aaf1c926b3d0b07f13157c56
qinju <13237404@qq.com>
2020-01-13 18:01:39 +0800

    1243

--- practice/src/main/java/com/blank/practice/PracticeApplication.java
+++ practice/src/main/java/com/blank/practice/PracticeApplication.java
@@ -1,9 +1,12 @@
 package com.blank.practice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
+@MapperScan(value = "com.blank.practice.mapper")
 public class PracticeApplication {
 
     public static void main(String[] args) {
