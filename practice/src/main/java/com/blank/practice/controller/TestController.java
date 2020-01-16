6c562c3a350322782dd776e990be87f4af686bf4
qinju <13237404@qq.com>
2020-01-16 17:54:14 +0800

    redis连接

--- practice/src/main/java/com/blank/practice/controller/TestController.java
+++ practice/src/main/java/com/blank/practice/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.blank.practice.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.blank.practice.entity.People;
 import com.blank.practice.service.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,9 +21,17 @@ import java.util.List;
 public class TestController {
     @Autowired
     private PeopleService peopleService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/")
-    public List<People> selectPeopleInfo(){
+    public List<People> selectPeopleInfo() {
         return peopleService.selectAll();
     }
 
+    @PostMapping("/people")
+    public Object addUser(@RequestBody People people){
+        stringRedisTemplate.opsForValue().set("people", JSON.toJSONString(people));
+        return "success";
+    }
 }
